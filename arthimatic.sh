c9a548dea730202012092dd6af62ee1a09f63a70
sukumarkindia424@gmail.com <sukumarkindia424@gmail.com>
2022-02-23 19:53:49 +0530

    added: fourth operation a%b+c logic

--- arthimatic.sh
+++ arthimatic.sh
@@ -5,4 +5,4 @@ read -p "enter third number: " c
 echo "value of a+b*c is :" $(($a+$b*$c))
 echo "value of a*b+c is : " $(($a*$b+$c))
 echo "value of c+a/b is ; " $(($c+$a/$b))
-
+echo "value of a%b+c is : " $(($a%$b+$c))
